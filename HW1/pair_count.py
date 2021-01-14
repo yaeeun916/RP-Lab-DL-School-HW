4672abf70f818da9f050941852dc5995e409f8a3
yaeeun916 <77432765+yaeeun916@users.noreply.github.com>
2021-01-14 22:15:43 +0900

    Update pair_count.py

--- HW1/pair_count.py
+++ HW1/pair_count.py
@@ -1,9 +1,9 @@
 #example 1
 
-def pair_count(input_list):  #input_list=['bcd', 'bce', 'cce']
+def pair_count(input_list):  #예: input_list=['bcd', 'bce', 'cce']
     pair_count=0
     for i in range(len(input_list)):
-        input_list[i]=set(list(input_list[i])) #input_list=[{'b', 'c', d'}, {'b', 'c', 'e'}, {'c', 'e'}]
+        input_list[i]=set(list(input_list[i])) #예: input_list=[{'b', 'c', d'}, {'b', 'c', 'e'}, {'c', 'e'}]
     for i in input_list:
         for j in input_list:
             if i!=j and len(list(i&j))==2: #서로 다른 두 set의 교집합의 원소가 2일 때
