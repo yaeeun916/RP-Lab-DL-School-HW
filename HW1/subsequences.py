0a29e40ce994200272d6c75aa74a4f8176fd5c6d
yaeeun916 <77432765+yaeeun916@users.noreply.github.com>
2021-01-15 10:09:01 +0900

    Add files via upload
    example 4

---
+++ HW1/subsequences.py
@@ -0,0 +1,22 @@
+#길이가 2일 때
+def subsequences2(iterable):
+    itList=list(iterable)
+    sub=()
+    for i in range(len(itList)):
+        for j in range(i+1, len(itList)):
+            sub=sub+(str(itList[i])+str(itList[j]),)
+    return sub
+            
+#길이가 3일 때
+def subsequences3(iterable):
+    itList=list(iterable)
+    sub=()
+    for i in range(len(itList)):
+        for j in range(i+1, len(itList)):
+            for k in range(j+1, len(itList)):
+                sub=sub+(str(itList[i])+str(itList[j])+str(itList[k]),)
+    return sub
+
+
+#길이가 n일 때로 확장하면?
+
