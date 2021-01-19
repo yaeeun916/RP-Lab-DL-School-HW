fd80070c759cef26bb3f1190ba7decdc0d7dbf33
yaeeun916 <77432765+yaeeun916@users.noreply.github.com>
2021-01-19 23:05:06 +0900

    Add files via upload

--- subsequences.py
+++ subsequences.py
@@ -16,3 +16,16 @@ def subseq(iterable, n):
 
     
 
+
+#종료조건을 하나로 줄일 수 있다
+def subseq(iterable, n):
+    itlist=[str(i) for i in iterable] #str으로 바꿔야 + 연산시 문제가 없다
+    if n==1:
+        return itlist 
+        
+    alist=[] #점화식 nCr=(n-1)C(r-1)+(n-2)C(r-1)+....+(r-1)C(r-1)
+    for i in range(len(iterable)-(n-1)):
+      for k in subseq(itlist[i+1:], n-1):
+        alist.append(itlist[i]+k)
+
+    return alist
